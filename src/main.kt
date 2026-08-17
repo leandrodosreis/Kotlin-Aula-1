72c8e99b55313a868bdf00d678b93eee58b5f1ae
LEANDRO DOS REIS FILHO <25203700@dsn127m1246734.cfp127aluno.local>
2026-08-17 16:57:17 -0300

    testes de kotlin

---
+++ src/main.kt
@@ -0,0 +1,86 @@
+//Comentarios
+
+/*
+Comentando
+trecho
+*/
+
+/*
+print
+fun main(){
+    println("Hello World!")
+}
+*/
+
+/*
+declaração de variaveis
+fun main(){
+    val pi = 3.14
+
+    var idade = 27
+
+    idade = 28
+}
+*/
+
+/*
+fun main(){
+    var idade : Int =27
+
+    idade = 28
+
+    var altura = 1.80
+
+    altura = 2.0
+}
+*/
+
+
+//
+//fun main() {
+//    val texto: String = "Bom dia, pessoal"
+//
+//    println(texto)
+//    println(texto.length)
+//    println(texto.uppercase())
+//    println(texto.contains("pessoal"))
+//}
+
+//fun main(){
+//    val nome = "joao"
+//
+//    val idade = 25
+//
+//    println("Bom dia $nome logo logo voce fara ${idade + 1} anos   ")
+//}
+
+//fun main(){
+//    val texto = """
+//        texto
+//        para
+//        3
+//    """
+//
+//    print(texto)
+//}
+
+
+//como definir um caracter como char ou seja caracter
+//fun main(){
+//
+//    val caracter = '*'
+//
+//    println(caracter)
+//}
+
+//fun main(){
+//    val bloqueado = false
+//
+//    println(bloqueado)
+//}
+
+fun main(){
+    val populacao = 80.60000909098555
+
+    println(populacao)
+}
